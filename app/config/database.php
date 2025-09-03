3620cd8ed27702077f475668e8927e38fbe6ef16
RON <h4ckw1z@gmail.com>
2025-09-03 13:51:28 +0800

    updated

--- app/config/database.php
+++ app/config/database.php
@@ -60,7 +60,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> getenv("DB_HOST"),
-    'port'		=> '3306',
+    'port'		=> getenv('DB_PORT'),
     'username'	=> getenv("DB_USER"),
     'password'	=> getenv("DB_PASS"),
     'database'	=> getenv("DB_NAME"),
